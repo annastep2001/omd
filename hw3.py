533cef7e14f7d2031609cce611f9957437b363b6
annastep2001 <80360986+annastep2001@users.noreply.github.com>
2021-10-29 17:37:42 +0300

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -1,38 +1,43 @@
+from collections import Counter
+
+
 class CountVectorizer():
     def __init__(self):
-        self.count_matrix = []
         self.feature_names = []
 
-    def fit_transform(self, raw_documents: list):
+    def fit(self, raw_documents):
         feature_names = set()
         count_matrix = []
         for line in raw_documents:
             for word in line.lower().split():
                 feature_names.add(word)
 
-        self.feature_names = list(feature_names)
-        dict_size = len(self.feature_names)
-        word_dict = dict(zip(self.feature_names, range(dict_size)))
+        self.feature_names = sorted(list(feature_names))
 
+    def transform(self, raw_documents):
+        count_matrix = []
+        all_words = dict.fromkeys(self.feature_names, 0)
         for line in raw_documents:
-            cur_line = [0] * dict_size
-            for word in line.lower().split():
-                ind = word_dict[word]
-                cur_line[ind] += 1
-            count_matrix.append(cur_line)
-        self.count_matrix = count_matrix
+            cur = Counter(line.lower().split())
+            cur.update(all_words)
+            cur = dict(sorted(cur.items(), key=lambda x: x[0]))
+            count_matrix.append(list(cur.values()))
         return count_matrix
+
+    def fit_transform(self, raw_documents: list):
+        self.fit(raw_documents)
+        return self.transform(raw_documents)
 
     def get_feature_names(self):
         return self.feature_names
 
 
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-]
-
-vectorizer = CountVectorizer()
-count_matrix = vectorizer.fit_transform(corpus)
-print(vectorizer.get_feature_names())
-print(count_matrix)
+if __name__ == "__main__":
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
+    print(count_matrix)
