38f655d9460f8bc35356f656fb2d1dd34d42c5f4
annastep2001 <80360986+annastep2001@users.noreply.github.com>
2021-11-29 20:28:41 +0300

    Add files via upload

---
+++ hw5/issue4.py
@@ -0,0 +1,61 @@
+from typing import List, Tuple
+
+
+def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
+    """
+    fit_transform(iterable)
+    fit_transform(arg1, arg2, *args)
+    """
+    if len(args) == 0:
+        raise TypeError('expected at least 1 arguments, got 0')
+
+    categories = args if isinstance(args[0], str) else list(args[0])
+    uniq_categories = set(categories)
+    bin_format = f'{{0:0{len(uniq_categories)}b}}'
+
+    seen_categories = dict()
+    transformed_rows = []
+
+    for cat in categories:
+        bin_view_cat = (int(b) for b in bin_format.format(1 << len(seen_categories)))
+        seen_categories.setdefault(cat, list(bin_view_cat))
+        transformed_rows.append((cat, seen_categories[cat]))
+    return transformed_rows
+
+
+def test_eq():
+    cities = ['Moscow', 'New York', 'Moscow', 'London']
+    actual = fit_transform(cities)
+    exp_transformed_cities = [
+        ('Moscow', [0, 0, 1]),
+        ('New York', [0, 1, 0]),
+        ('Moscow', [0, 0, 1]),
+        ('London', [1, 0, 0]),
+    ]
+
+    assert actual == exp_transformed_cities
+
+
+def test_notin():
+    cities = ['Moscow', 'New York', 'Moscow', 'London']
+    actual = fit_transform(cities)
+    exp_transformed_cities = [
+        ('Moscow', [0, 0, 1]),
+        ('New York', [0, 1, 0]),
+        ('Moscow', [0, 0, 1]),
+        ('London', [1, 0, 0]),
+    ]
+    notin = ('Moscow', [0, 0, 2])
+    assert notin not in exp_transformed_cities
+
+
+def test_animals():
+    animals = ['cat', 'dog', 'tiger']
+    expected = [('cat', [0, 0, 1]), ('dog', [0, 1, 0]), ('tiger', [1, 0, 0])]
+    actual = fit_transform(animals)
+    assert actual == expected
+
+
+def test_nan():
+    actual = fit_transform(list())
+    assert actual is None
